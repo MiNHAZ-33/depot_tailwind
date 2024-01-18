eba77e3d480ef7901f3f9386585eaa3102bedb30
MiNHAZ-33 <miminhaz33@gmail.com>
2024-01-18 20:20:42 +0600

    adding localisation in order page

---
+++ app/mailers/order_mailer.rb
@@ -0,0 +1,26 @@
+class OrderMailer < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.order_mailer.received.subject
+
+  default from: "Minhaz <miminhaz33@gmail.com>"
+
+  def received(order)
+   @order = order
+
+   mail to: order.email, subject: 'Book store confirmation'
+  end
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.order_mailer.shipped.subject
+  #
+  def shipped
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
+  end
+end
